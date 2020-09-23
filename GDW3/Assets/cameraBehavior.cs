466a642e4379a0747a1a2dfe1d6d54df9f6977a3
NuclearCosmo <mac.macivor@uoit.net>
2020-09-23 12:14:15 -0400

    Initial commit
    Let's see if this works

---
+++ GDW3/Assets/cameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour
+{
+
+    public Transform characterPos;
+    private Vector3 cameraOffset;
+    [Range(0.001f, 1.0f)]
+    public float smoothfactor = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - characterPos.position; //Remembers the offset you had put in unity
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = characterPos.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothfactor);
+    }
+}
