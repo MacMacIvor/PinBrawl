3c615c6e8bb21bc95c2c19f1352afe1e150394ca
NuclearCosmo <mac.macivor@uoit.net>
2020-12-07 16:36:55 -0500

    Fixed the issue with going from tutorial to the first level

--- GDW3/Assets/Scripts/SceneSwitching/toNextLevel.cs
+++ GDW3/Assets/Scripts/SceneSwitching/toNextLevel.cs
@@ -16,7 +16,7 @@ public class toNextLevel : MonoBehaviour
         Destroy(this);
     }
 
-    int levelToLoad = 0;
+    static int levelToLoad = 0;
 
     // Start is called before the first frame update
     void Start()
