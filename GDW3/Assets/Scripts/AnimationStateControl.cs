19ef20e5baee7e83e2d3c4146658e2655fddce52
Thaidan <thaidan.goguenbogdanis@ontariotechu.net>
2021-04-08 21:46:19 -0400

    Cutscene Added
    Added a cutscene at the opening that you can skip with spacebar.

---
+++ GDW3/Assets/Scripts/AnimationStateControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateControl : MonoBehaviour
+{
+    Animator animator;
+    float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 5.5f && timer < 7.85f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if (timer >= 7.85f)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (timer > 12.0f)
+        {
+            //Next scene
+        }
+    }
+}
